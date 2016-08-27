7c6cc190cb39953911c493218b0fbbd2aaf4b4c1
Bogdan Chadkin <trysound@yandex.ru>
2016-08-28 00:11:53 +0300

    Remove dead code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 'use strict';
 
 const { rollup } = require('rollup');
-const debounce = require('debounce');
-const dependencies = new Map();
-const changedParents = new Set();
 const SOURCEMAPPING_URL = 'sourceMappingURL';
-const WAIT = 25;
-const touchParents = debounce(() => {
-    const now = Date.now(); // gives better performance then new Date()
-    for (let idx = 0, lst = changedParents.values(); idx < lst.length; idx += 1) {
-        fs.utimes(parent, now, now);
-    }
-    changedParents.clear();
-}, WAIT);
 
 function createRollupPreprocessor (args, options = {}, logger) {
-    let log = logger.create('preprocessor.rollup');
+    const log = logger.create('preprocessor.rollup');
 
     return (content, file, done) => {
         log.debug('Processing "%s".', file.originalPath);
@@ -23,48 +12,22 @@ function createRollupPreprocessor (args, options = {}, logger) {
         try {
             options.entry = file.originalPath;
 
-            rollup(options)
-                .then(bundle => {
+            rollup(options).then(bundle => {
+                let { code, map } = bundle.generate(options);
 
-                    // Map this file to the dependencies that Rollup just
-                    // compiled.
-                    dependencies.set(
-                        file.originalPath,
-                        bundle.modules
-                            .map(b => b.id)
-                            .filter(op => op !== file.originalPath)
-                    );
+                if (options.sourceMap === 'inline') {
+                    code += '\n//# ' + SOURCEMAPPING_URL + '=' + map.toUrl();
+                }
 
-                    // Work backwards from dependencies to see what
-                    // relies on this file, then trigger a recompilation of
-                    // it.
-                    for (let i = 0, list = dependencies.entries(); i < list.length; i += 1) {
-                        const entry = list[i];
-                        const parent = entry[0];
-                        const dependList = entry[1];
-                        if (dependList.includes(file.originalPath)) {
-                            log.debug(" \n%s depends on \n\t%s\n    Recompiling it.", parent, file.originalPath);
-                            changedParents.add(parent);
-                            touchParents();
-                        }
-                    }
+                if (options.sourceMap) {
+                    file.sourceMap = map;
+                }
 
-                    let { code, map } = bundle.generate(options);
-
-                    if (options.sourceMap === 'inline') {
-                        code += '\n//# ' + SOURCEMAPPING_URL + '=' + map.toUrl();
-                    }
-
-                    if (options.sourceMap) {
-                        file.sourceMap = map;
-                    }
-
-                    done(null, code);
-                })
-                .catch(error => {
-                    log.error('%s\n at %s\n%s', error.message, file.originalPath, error.stack);
-                    done(error);
-                });
+                done(null, code);
+            }).catch(error => {
+                log.error('%s\n at %s\n%s', error.message, file.originalPath, error.stack);
+                done(error);
+            });
 
         }
         catch (error) {
